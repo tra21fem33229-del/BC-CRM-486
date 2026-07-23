b029e3a366b360df6e374b52b59e7ffa5b3278ef
tra21fem33229-del <tra21fem33229@fsb.edu.vn>
2026-07-23 11:07:39 +0700

    Delete crm_486-main directory

--- crm_486-main/pages/api/session.js
+++
@@ -1,6 +0,0 @@
-import { verifySessionToken } from '../../lib/auth';
-
-export default async function handler(req, res) {
-  const token = req.cookies?.session;
-  return res.status(200).json({ authed: verifySessionToken(token) });
-}
